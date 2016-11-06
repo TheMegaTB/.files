0ec6fc083601d172a08a09b70e37895a3e8f2edf
Til Blechschmidt <til@blechschmidt.de>
2016-11-06 14:39:06 +0100

    Added auto-login

--- patches/patch.sh
+++ patches/patch.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 patch /usr/share/polkit-1/actions/org.freedesktop.upower.policy < upower_permission.patch
 patch /etc/inittab < inittab_runlevel_full.patch
+patch /etc/inittab < automatic_login.patch
