d5df7b858ad34581d775b41b02d2b45640a8d21b
Til Blechschmidt <til@blechschmidt.de>
2017-03-21 22:47:12 +0100

    Added google apps

--- home/themegatb/.scripts/launch_if_not_running.sh
+++ home/themegatb/.scripts/launch_if_not_running.sh
@@ -6,6 +6,13 @@ ERRLVL=$?
 
 echo $ERRLVL
 
+if [ $ERRLVL -eq 1 ]; then
+    echo "Alternative method"
+    pgrep -f GCal > /dev/null
+    ERRLVL=$?
+    echo $ERRLVL
+fi
+
 if [ $ERRLVL -eq 0 ]; then
     echo "Already running."
     exit 1
